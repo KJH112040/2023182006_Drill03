9a7d432548ffddbcd9eb3753cd6fc0612643744a
KJH04_ <10wjdgp04@gmail.com>
2024-09-24 10:17:46 +0900

    원 운동

--- character_grass.py
+++ character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -13,11 +14,17 @@ def run_rectangle():
 
 def run_circle():
     print('CIRCLE')
+
+    r = 300
+    cx = 800 // 2
+    cy = 600 // 2
+    for d in range(0, 360):
+        x = r * math.cos(math.radians(d)) + cx
+        y = r * math.sin(math.radians(d)) + cy
     
-    
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(1)
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.1)
     
     pass
 
