b95eb38a5947fb3bc868930e0ff111bbeab4822f
KJH04_ <10wjdgp04@gmail.com>
2024-09-24 09:55:22 +0900

    rectangle, circle 무한 반복 작동
    그 와중 이미지 로드 왜 안 됐는지 알게됨 .png 빼먹음 ㅋㅋㅋ

--- character_grass.py
+++ character_grass.py
@@ -1,15 +1,17 @@
 from pico2d import *
 
 def run_rectangle():
+    print('RECTANGLE')
     pass
 
 def run_circle():
+    print('CIRCLE')
     pass
 
 open_canvas()
 
 # fill here
-grass = pico2d.load_image('grass')
+grass = pico2d.load_image('grass.png')
 character = load_image('character.png')
 
 
