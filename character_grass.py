15cf030e3c127eac8c1c907f8446c703cd4dc009
KJH04_ <10wjdgp04@gmail.com>
2024-09-24 09:53:05 +0900

    왜 이미지 로드 안되지

--- character_grass.py
+++ character_grass.py
@@ -1,7 +1,20 @@
 from pico2d import *
 
+def run_rectangle():
+    pass
+
+def run_circle():
+    pass
+
 open_canvas()
 
 # fill here
+grass = pico2d.load_image('grass')
+character = load_image('character.png')
+
+
+while True:
+    run_rectangle()
+    run_circle()
 
 close_canvas()
